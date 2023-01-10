5b74735346749f102458e2aac14d0bc6ba374654
Isu Shin <isu010614@naver.com>
2023-01-10 15:15:21 +0900

    refactor: 레퍼런스 생성자 주입

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,13 +1,23 @@
 package hello.core;
 
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 
+/**
+ * 앱 실제 동작에 필요한 구현 객체를 생성해주는 클래스
+ */
 public class AppConfig {
 
+    // 생성한 객체 인스턴스의 레퍼런스를 '생성자를 통해서 주입'해줌
     public MemberService memberService() {
-        return new MemberServiceImpl();
+        return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
-
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
 }
