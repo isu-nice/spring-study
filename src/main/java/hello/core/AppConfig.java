58f0255d81d6c21d64653b4c7841867ae9ca2c85
Isu Shin <isu010614@naver.com>
2023-01-11 14:55:10 +0900

    refactor: 스프링 활용으로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,25 +8,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * 앱 실제 동작에 필요한 구현 객체를 생성해주는 클래스
+ * 설정 정보에 대한 내용
  */
+@Configuration
 public class AppConfig {
 
-    // 생성한 객체 인스턴스의 레퍼런스를 '생성자를 통해서 주입'해줌
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
-    }
+    } // 생성한 객체 인스턴스의 레퍼런스를 '생성자를 통해서 주입'해줌
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy(); // 할인 정책 변경이 쉬워짐
