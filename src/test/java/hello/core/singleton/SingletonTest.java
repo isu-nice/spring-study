10380a4f4e00eec2d2e9348ade60dada22ad6282
Isu Shin <isu010614@naver.com>
2023-01-12 18:21:17 +0900

    test: 싱글톤 패턴 적용한 객체 사용 테스트

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -18,12 +18,29 @@ public class SingletonTest {
 
         // 2. 조회: 호출할 때마다 객체를 생성
         MemberService memberService2 = appConfig.memberService();
-        
+
         // 3. 참조값이 다른 것을 확인
         System.out.println("memberService1 = " + memberService1);
         System.out.println("memberService2 = " + memberService2);
 
         // 4. memberService1 != memberService2
         assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+        // 1. 객체 여러번 생성
+        SingletonService service1 = SingletonService.getInstance();
+        SingletonService service2 = SingletonService.getInstance();
+
+        // 2. 참조값이 같은 것을 확인
+        System.out.println("service1 = " + service1);
+        System.out.println("service2 = " + service2);
+
+        // 3. service1 == service2
+        assertThat(service1).isSameAs(service2);
+        // isSameAs -> 객체 인스턴스를 비교
+        // isEqualTo -> equals 메서드를 의미
     }
 }
