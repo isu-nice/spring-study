82fe41adaff78dd1e389935f519f40e0a0d27de8
Isu Shin <isu010614@naver.com>
2023-01-22 00:25:30 +0900

    chore: indent 정리

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -5,13 +5,13 @@ import hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
-public class RateDiscountPolicy implements DiscountPolicy{
+public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
 
     @Override
     public int discount(Member member, int price) {
-        if(member.getGrade() == Grade.VIP){
+        if (member.getGrade() == Grade.VIP) {
             return price * discountPercent / 100;
         } else {
             return 0;
