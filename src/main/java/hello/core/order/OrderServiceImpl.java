2ae65a9bd86e6f97b51868911e064c1c40b52bf2
Isu Shin <isu010614@naver.com>
2023-01-27 16:53:19 +0900

    fix: @RequiredArgsConstructor 사용에 따른 생성자 주석처리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,15 +3,27 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // 생성자 만들어주는 어노테이션
 public class OrderServiceImpl implements OrderService {
 
     // 인터페이스에만 의존하게 된다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    /* @RequiredArgsConstructor 사용 -> 생성자 자동으로 만들어주기 때문에 아래 코드 삭제 가능
+
+    // setter 주입하면 생략가능 -> 그렇지만 DI 방식이 낫다!
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+     */
 
     /* DI 방식 사용 (final 붙여줌) -> setter 사용하지 않을 것이기 때문에 삭제해줌
     @Autowired
@@ -24,13 +36,6 @@ public class OrderServiceImpl implements OrderService {
         this.discountPolicy = discountPolicy;
     }
      */
-
-    // setter 주입하면 생략가능 -> 그렇지만 DI 방식이 낫다!
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
