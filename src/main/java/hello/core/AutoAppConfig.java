af1fab6e19e22fc76fae1106a9d88b072d0267aa
Isu Shin <isu010614@naver.com>
2023-01-27 16:13:24 +0900

    comment: 학습용 주석 추가

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -15,7 +15,8 @@ import static org.springframework.context.annotation.ComponentScan.*;
 )
 public class AutoAppConfig {
 
-    /*
+    /* 수동 빈 등록 시 자동 등록 빈과 충돌이 일어날 수 있음
+    -> 최근 스프링 부트는 충돌나면 오류가 발생하게 됨
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();
--- src/test/java/hello/core/autowired/AutowiredTest.java
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -17,20 +17,21 @@ public class AutowiredTest {
     }
 
     static class TestBean {
+        // 자동 주입할 대상이 없는 경우 (Member 는 스프링 빈이 아님)
 
-        // 호출 안됨
+        // @Autowired(required=false) -> 호출 안됨
         @Autowired(required = false)
         public void setNoBean1(Member noBean1){
             System.out.println("noBean1 = " + noBean1);
         }
 
-        // null 호출
+        // @Nullable -> null 호출
         @Autowired
         public void setNoBean2(@Nullable Member noBean2){
             System.out.println("noBean2  = " + noBean2);
         }
 
-        // Optional.empty 호출
+        // Optional<> -> Optional.empty 호출
         @Autowired
         public void setNoBean3(Optional<Member> noBean3) {
             System.out.println("noBean3 = " + noBean3);
